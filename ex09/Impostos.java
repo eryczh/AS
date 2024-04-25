72d06d26a14e7a6482504b1f6d2841d5a4d866f2
caiotelesz <caioteles1267@gmail.com>
2024-04-25 18:35:10 -0300

    Exer 09 e 11

--- ex09/Impostos.java
+++ ex09/Impostos.java
@@ -1,24 +1,25 @@
 package ex09;
 
 public class Impostos {
-    class ImpostoIP1 implements ImpostoStrategy {
+    
+    public static class ImpostoIP1 implements ImpostoStrategy {
         @Override
         public double calcularImposto(double orcamento) {
             return orcamento * 0.1;
         }
     }
 
-    class ImpostoIP2 implements ImpostoStrategy {
+    public static class ImpostoIP2 implements ImpostoStrategy {
         @Override
         public double calcularImposto(double orcamento) {
             return orcamento * 0.15;
         }
     }
 
-    class ImpostoIP3 implements ImpostoStrategy {
+    public static class ImpostoIP3 implements ImpostoStrategy {
         @Override
         public double calcularImposto(double orcamento) {
             return orcamento * 0.25;
-        } 
+        }
     }
 }
--- ex09/Main.java
+++ ex09/Main.java
@@ -2,13 +2,33 @@ package ex09;
 
 public class Main {
     public static void main(String[] args) {
-        double orcamento = 1000;
 
-        ImpostoStrategy impostoStrategy = new ImpostoIP1(); 
+        // Imposto 1
+        CalculadoraImposto calculadora1 = new CalculadoraImposto(new Impostos.ImpostoIP1());
 
-        CalculadoraImposto calculadoraImposto = new CalculadoraImposto(impostoStrategy);
+        double orcamento1 = 3000.0;
 
-        double impostoCalculado = calculadoraImposto.calcularImposto(orcamento);
-        System.out.println("Imposto calculado: " + impostoCalculado);
+        double impostoCalculado1 = calculadora1.calcularImposto(orcamento1);
+
+        System.out.println("Imposto 1 calculado: " + impostoCalculado1);
+
+        // Imposto 2
+        CalculadoraImposto calculadora2 = new CalculadoraImposto(new Impostos.ImpostoIP2());
+
+        double orcamento2 = 3000.0;
+
+        double impostoCalculado2 = calculadora2.calcularImposto(orcamento2);
+
+        System.out.println("Imposto 2 calculado: " + impostoCalculado2);
+
+        // Imposto 3
+        CalculadoraImposto calculadora3 = new CalculadoraImposto(new Impostos.ImpostoIP3());
+
+        double orcamento3 = 3000.0;
+
+        double impostoCalculado3 = calculadora3.calcularImposto(orcamento3);
+
+        System.out.println("Imposto 3 calculado: " + impostoCalculado3);
+
     }
 }
